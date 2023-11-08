089e37e31a6b650387f61ead59c3dc3a94a598fa
dementyevvv <dementyevvv@novotelecom.ru>
2023-11-08 10:03:32 +0700

    Initial commit

---
+++ src/test/java/dev/mycalories/myCalories/MyCaloriesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mycalories.myCalories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCaloriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
